8596e8c0d356cd7ea8f620ca6a4bd7d4b146b12b
GanbarDadashzada <qenberdd@gmail.com>
2023-01-23 23:11:35 +0400

    BE-9 : Mappers were updated

--- src/main/java/com/carzapi/digital/mapper/CriteriaMapper.java
+++ src/main/java/com/carzapi/digital/mapper/CriteriaMapper.java
@@ -2,11 +2,9 @@ package com.carzapi.digital.mapper;
 
 import com.carzapi.digital.dao.entity.AnnouncementEntity;
 import com.carzapi.digital.model.dto.LightAnnouncementDto;
-import com.carzapi.digital.model.dto.PageableDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-import org.springframework.data.domain.Page;
 
 @Mapper
 public interface CriteriaMapper {
@@ -18,6 +16,8 @@ public interface CriteriaMapper {
     @Mapping(target = "motorSize",source = "motorSize")
     @Mapping(target = "distanceType",source = "distanceType")
     @Mapping(target = "distanceUnit",source = "distanceUnit")
+    @Mapping(target = "year",source = "year")
+    @Mapping(target = "privileges",source = "privileges")
     @Mapping(target = "createdAt", source = "createdAt")
     LightAnnouncementDto announcementToLightDto (AnnouncementEntity announcementEntity);
 
