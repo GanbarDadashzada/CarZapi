4afb1f8238bbc0fccad66520a0836312c4afff3d
GanbarDadashzada <qenberdd@gmail.com>
2023-01-27 16:51:00 +0400

    BE-10 : Controller was updated

--- src/main/java/com/carzapi/digital/controller/DictionaryController.java
+++ src/main/java/com/carzapi/digital/controller/DictionaryController.java
@@ -1,5 +1,6 @@
 package com.carzapi.digital.controller;
 
+import com.carzapi.digital.dao.entity.PrivilegeEntity;
 import com.carzapi.digital.model.dto.DictionaryDto;
 import com.carzapi.digital.service.DictionaryService;
 import lombok.RequiredArgsConstructor;
@@ -31,10 +32,15 @@ public class DictionaryController {
 
     @GetMapping("/brands")
     public List<DictionaryDto> getBrand() { return dictionaryService.getBrand(); }
+
     @GetMapping("/equipments")
     public List<DictionaryDto> getEquipments() { return dictionaryService.getEquipment(); }
+
     @GetMapping("/conditions")
     public List<DictionaryDto> getConditions() { return dictionaryService.getCondition(); }
+
+    @GetMapping("/privileges")
+    public List<DictionaryDto> getPrivileges() { return dictionaryService.getPrivileges(); }
 
     @GetMapping("/brands/{id}/models")
     public List<DictionaryDto> getModels(@PathVariable Long id) {
