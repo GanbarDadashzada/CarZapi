a9237e34f8055ea6581dd836d9a42db086b6f1c2
GanbarDadashzada <qenberdd@gmail.com>
2023-02-07 22:48:54 +0400

    BE-12 : Mapper was updated

--- src/main/java/com/carzapi/digital/mapper/VerificationMapper.java
+++ src/main/java/com/carzapi/digital/mapper/VerificationMapper.java
@@ -1,20 +1,21 @@
 package com.carzapi.digital.mapper;
 
 import com.carzapi.digital.dao.entity.VerificationEntity;
+import com.carzapi.digital.dao.entity.embedded.VerificationKey;
 import com.carzapi.digital.model.dto.VerificationDto;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 @Mapper
 public interface VerificationMapper {
     VerificationMapper INSTANCE = Mappers.getMapper(VerificationMapper.class);
-    @Mapping(target = "token", source = "token")
-    @Mapping(target = "email",
-            expression = "java(verificationEntity.getId().getEmail())")
-    VerificationDto entityToDto (VerificationEntity verificationEntity);
 
-    @Mapping(target = "token", source = "verificationDto.token")
-    @Mapping(target = "id.email", source = "verificationDto.email")
+    @Mapping(target = "token", source = "token")
+    @Mapping(target = "id.email", source = "email")
+    @Mapping(target = "id.type", source = "type")
     VerificationEntity dtoToEntity (VerificationDto verificationDto);
+
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "type", source = "type")
+    VerificationKey dtoToKey (VerificationDto verificationDto);
 }
